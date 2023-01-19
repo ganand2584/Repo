6c5d3d809152930ec6cad5b5fc7f1279529eb6c1
ganand2584 <prem.s.g.anand@gmail.com>
2023-01-19 12:34:07 +0530

    hello

---
+++ Data_Type.java
@@ -0,0 +1,36 @@
+package org.datatype;
+public class Data_Type {
+public static void main(String[] args) {
+		byte b = 123;
+		System.out.println("byte value is " +  b);//concatenate
+		shorts = 12345;
+		System.out.println("short value is " + s);
+		int i = 1234567890;
+		System.out.println("integer value is "  + i);
+		long l = 1234567890123456789l;
+		System.out.println("Long value is " + l);
+		float f = 12345678901234567890123456789f;
+		System.out.println("Float value is " + f);
+		float f1 = 1.2345678901234567890123f;
+		System.out.println("Float value is " + f1);
+		double d = 1234567890;
+		System.out.println("Double value is "  + d);
+		char c = 'a'; // with single quote
+		System.out.println("Char value is " + c);
+		char c1 = 'A'; // with single quote
+		char c2 = '1';
+		char c3 = '#';
+		System.out.println("Char value is " + c1);
+		System.out.println("Char value is " + c2);
+		System.out.println("char value is "  + c3);
+		boolean b1 = true;
+		boolean b2 = false;
+		System.out.println("bool value is " + b1);
+		System.out.println("bool value is " + b2);
+		char c4 = 'a';
+		char c5 = 'b';
+		System.out.println(c4==c5);
+		String str = "Prem is Learning Selenium @ 7.45am" ;
+		System.out.println("String value is" + str);
+		}
+}
